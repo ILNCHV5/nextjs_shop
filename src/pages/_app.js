4c0413c89f6dedbee81ba94c39c6465e688756de
Iliyan Nachev <iliyan.nachev@altscale.com>
2023-11-12 21:10:00 +0200

    style engine wrap

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,16 +2,17 @@ import '@/styles/globals.css';
 import Head from 'next/head';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../styles/theme.js';
+import { StyledEngineProvider } from '@mui/material/styles';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <StyledEngineProvider injectFirst>
       <Head>
         <link rel='icon' href='/images/favicon/favicon.ico' />
       </Head>
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
       </ThemeProvider>
-    </>
+    </StyledEngineProvider>
   );
 }
